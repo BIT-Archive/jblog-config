f8c1fdee4339723a49ab4dcbdf35b47bb26bd925
jihunhong <redgee49@gmail.com>
2019-06-05 17:23:55 +0900

    properties 수정

--- jblog5/src/main/java/com/cafe24/jblog2/config/WebConfig.java
+++ jblog5/src/main/java/com/cafe24/jblog2/config/WebConfig.java
@@ -13,12 +13,9 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.cafe24.jblog2.security.AuthGlobalInterceptor;
 import com.cafe24.jblog2.security.AuthLoginInterceptor;
@@ -28,103 +25,71 @@ import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer{
 	
 
 	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		
-		resolver.setExposeContextBeansAsAttributes(true);
-		
-		
-		return resolver;
-	}
-	
-	// Default Servlet handler
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-	    configurer.enable();
-	}
-
-	
-	// Message Converter
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(mappingJackson2HttpMessageConverter());
-		converters.add(stringHttpMessageConverter());
-	}
-	
-
 	public AuthUserHandlerMethodArgumentResolver authUserHandlerMethodArgumentResolver() {
 		return new AuthUserHandlerMethodArgumentResolver();
 	}
-	
-	
-	//
-	// AddArgumentResolvers
-	//
+
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(authUserHandlerMethodArgumentResolver());
 	}
 
+	/*
+	 * Intercepter
+	 */
+	@Bean
+	public AuthGlobalInterceptor authglobalIntercetpor() {
+		return new AuthGlobalInterceptor();
+	}
+
+	@Bean
+	public AuthLoginInterceptor authLoginIntercetpor() {
+		return new AuthLoginInterceptor();
+	}
+
+	@Bean
+	public AuthLogoutInterceptor authLogoutIntercetpor() {
+		return new AuthLogoutInterceptor();
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry
-			.addInterceptor(authLoginInterceptor())
-			.addPathPatterns("/user/auth");
-		
-		registry
-		.addInterceptor(authLoginOutInterceptor())
-		.addPathPatterns("/user/logout");
-		
-		registry
-		.addInterceptor(authGlobalInterceptor())
-		.addPathPatterns("/**")
-		.excludePathPatterns("/user/auth")
-		.excludePathPatterns("/user/logout")
-		.excludePathPatterns("/assets/**");
+		registry.addInterceptor(authLoginIntercetpor()).addPathPatterns("/user/auth");
+		registry.addInterceptor(authLogoutIntercetpor()).addPathPatterns("/user/logout");
+		registry.addInterceptor(authglobalIntercetpor()).addPathPatterns("/**").excludePathPatterns("/user/auth")
+				.excludePathPatterns("/user/logout").excludePathPatterns("/assets/**").excludePathPatterns("/user/**");
 	}
-	
-	public AuthLoginInterceptor authLoginInterceptor() {
-		return new AuthLoginInterceptor();
-	}
-	public AuthLogoutInterceptor authLoginOutInterceptor() {
-		return new AuthLogoutInterceptor();
-	}
-	
-	
-	@Bean
-	public AuthGlobalInterceptor authGlobalInterceptor() {
-		return new AuthGlobalInterceptor();
-	}
-	
-	
+
 	@Bean
 	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
-				.indentOutput(true)
-				.dateFormat(new SimpleDateFormat("yyyy-MM-dd"))
-				.modulesToInstall(new ParameterNamesModule() );
-		
-		
-		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-		
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder().indentOutput(true)
+				.dateFormat(new SimpleDateFormat("yyyy-mm-dd")).modulesToInstall(new ParameterNamesModule());
+
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(builder.build());
 		converter.setSupportedMediaTypes(Arrays.asList(new MediaType("application", "json", Charset.forName("UTF-8"))));
-		
 		return converter;
 	}
-	
+
 	@Bean
 	public StringHttpMessageConverter stringHttpMessageConverter() {
 		StringHttpMessageConverter converter = new StringHttpMessageConverter();
-		converter.setSupportedMediaTypes(Arrays.asList(new MediaType("text","html", Charset.forName("UTF-8"))));
-		
+		converter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "html", Charset.forName("UTF-8"))));
 		return converter;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(mappingJackson2HttpMessageConverter());
+		converters.add(stringHttpMessageConverter());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**").addResourceLocations("file:/mysite-uploads/");
 	}
 }
 	
