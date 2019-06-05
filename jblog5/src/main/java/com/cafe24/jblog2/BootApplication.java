5facc4b4c1c1887de54dad0e7b103380bffaeb72
jihunhong <redgee49@gmail.com>
2019-06-05 15:31:50 +0900

    스프링부트 테스트

---
+++ jblog5/src/main/java/com/cafe24/jblog2/BootApplication.java
@@ -0,0 +1,13 @@
+package com.cafe24.jblog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args); 
+	}
+
+}
